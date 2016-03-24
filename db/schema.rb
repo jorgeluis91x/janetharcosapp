61de88359753e3d6ebb6602838ec28521c77c370
jorgeluis91x <jorge_luis91x@hotmail.com>
2016-03-24 11:43:12 -0500

    change to spanish in  cliente, category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320172045) do
+ActiveRecord::Schema.define(version: 20160324162846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160320172045) do
     t.date     "birthdate"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
